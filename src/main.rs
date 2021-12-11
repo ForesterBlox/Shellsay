2f099ed30fb750747c52ca8b0a627eee1d7cd07b
ForesterBlox <83901459+ForesterBlox@users.noreply.github.com>
2021-12-11 09:22:00 +0700

    Added error message

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,9 @@ use std::env;
 
 fn main()
 {
-    let args: Vec<String> = env::args().collect();
-    let text = &args[1];
+    let mut args = env::args();
+    let _ = args.next();
+    let text = args.next().expect("It didn't say anything");
     println!("\n               < {} >\n               /\n___________   /\n( >        ) /\n(  ______  )\n(__________)\n
              ", text);
 }
