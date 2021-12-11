bb77acf2ed44ceea8a939aff11296e2cb3e974c3
ForesterBlox <83901459+ForesterBlox@users.noreply.github.com>
2021-12-11 07:19:20 +0700

    bad thing

--- main.rs
+++
@@ -1,9 +0,0 @@
-use std::env;
-
-fn main()
-{
-    let args: Vec<String> = env::args().collect();
-    let text = &args[1];
-    println!("\n               < {} >\n               /\n___________   /\n( >        ) /\n(  ______  )\n(__________)\n
-             ", text);
-}
